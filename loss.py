c534e2ac6cf86f44ccf7facbf5d10377f4cb5edf
ephe <ephe@student.42.fr>
2022-10-17 14:58:54 +0200

    refacto-loss-* : clean

--- loss.py
+++ loss.py
@@ -4,12 +4,44 @@
 import numpy as np
 
 
-class LossFunction:
+class LossFunctions:
+    def __init__(self, eps=1e-15):
+        self.eps = eps
+        self.loss_functions = {
+            "binary_cross_entropy": (
+                BinaryCrossEntropy.binary_cross_entropy,
+                BinaryCrossEntropy.binary_cross_entropy_prime,
+            ),
+            "categorical_cross_entropy": (
+                CategoricalCrossEntropy.categorical_cross_entropy,
+                CategoricalCrossEntropy.categorical_cross_entropy_prime,
+            ),
+        }
+
+
+class BinaryCrossEntropy:
     def __init__(self):
-        self.eps = 1e-15
+        pass
 
-    def cross_entropy(self, y, y_hat):
-        return -np.sum(y.T @ np.log(y_hat + self.eps)) / y.shape[0]
+    def binary_cross_entropy(y, y_hat):
+        return (
+            -np.sum(
+                y.T @ np.log(y_hat + LossFunctions.eps)
+                + (1 - y).T @ np.log(1 - y_hat + LossFunctions.eps)
+            )
+            / y.shape[0]
+        )
 
-    def cross_entropy_prime(self, y, y_hat):
+    def binary_cross_entropy_prime(y, y_hat):
         return y_hat - y
+
+
+class CategoricalCrossEntropy:
+    def __init__(self):
+        pass
+
+    def categorical_cross_entropy(y, y_hat):
+        return -np.sum(y.T @ np.log(y_hat + LossFunctions.eps)) / y.shape[0]
+
+    def categorical_cross_entropy_prime(y, y_hat):
+        return -y / (y_hat + LossFunctions.eps)
